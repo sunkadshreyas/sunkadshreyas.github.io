e6e774df77e1392397c7376ac61ee7f3987c701f
Shreyas Sunkad <sunkad.shreyas@gmail.com>
2024-06-01 23:41:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-  
-  
-  
-  
-function openPage(event,tab){
-    var i;
-    var x = document.getElementsByClassName("tabcontent")
-    for(i=0;i<x.length;i++){
-      x[i].style.display = "none";
-    }
-    document.getElementById(tab).style.display = "block";
-    links = document.getElementsByClassName("item");
-    for(i=0;i<links.length;i++){
-        links[i].className = links[i].className.replace("active","");
-    }
-    event.currentTarget.className += " active";
-  }
-  document.getElementById("defaultOpen").click()
+
